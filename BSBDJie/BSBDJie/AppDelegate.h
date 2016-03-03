f95f0430ba7b4c77bda64f4e6b9abd80560f162b
shengguoqiang <2420548376@qq.com>
2016-03-03 17:13:12 +0800

    提交基础工程

---
+++ BSBDJie/BSBDJie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BSBDJie
+//
+//  Created by shengguoqiang on 16/3/3.
+//  Copyright © 2016年 shengguoqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BSBDJie/BSBDJie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BSBDJie
+//
+//  Created by shengguoqiang on 16/3/3.
+//  Copyright © 2016年 shengguoqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
